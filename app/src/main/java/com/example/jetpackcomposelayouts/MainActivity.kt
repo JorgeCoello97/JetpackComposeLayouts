c1362bf9f0a98957d2bf99f0a85e5cbe2a62eba3
Jorge Coello <56586359+JorgeCoello97@users.noreply.github.com>
2021-08-29 23:01:54 +0200

    5

--- app/src/main/java/com/example/jetpackcomposelayouts/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposelayouts/MainActivity.kt
@@ -3,26 +3,25 @@ package com.example.jetpackcomposelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.jetpackcomposelayouts.ui.theme.JetpackComposeLayoutsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,34 +35,99 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(){
+fun MyApp() {
+    val openDialog = remember { mutableStateOf(false) }
+
+    if (openDialog.value) {
+        AlertDialog(
+            onDismissRequest = {
+                openDialog.value = false
+            },
+            title = {
+                Text(text = "Title")
+            },
+            text = {
+                Text(
+                    "This area typically contains the supportive text " +
+                            "which presents the details regarding the Dialog's purpose."
+                )
+            },
+            buttons = {
+                Row(
+                    modifier = Modifier.padding(all = 8.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { openDialog.value = false }
+                    ) {
+                        Text("Dismiss")
+                    }
+                }
+            }
+        )
+    }
     Scaffold(
         topBar = {
-           TopAppBar(
-               title = {
-               Text(text = "Layouts Codelab")
-               },
-               actions = {
-                   IconButton(onClick = { /*TODO*/ }) {
-                       Icon(Icons.Filled.Favorite, contentDescription = null)
-                   }
-               }
-           )
+            TopAppBar(
+                title = {
+                    Text(text = "Layouts Codelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                    IconButton(onClick = { openDialog.value = true }) {
+                        Icon(Icons.Filled.Star, contentDescription = null)
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomNavigation() {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Star, contentDescription = null) },
+                    selected = false,
+                    label = { Text("START") },
+                    onClick = {}
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Warning, contentDescription = null) },
+                    selected = false,
+                    label = { Text("START") },
+                    onClick = {}
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Send, contentDescription = null) },
+                    selected = false,
+                    label = { Text("START") },
+                    onClick = {}
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Refresh, contentDescription = null) },
+                    selected = false,
+                    label = { Text("START") },
+                    onClick = {}
+                )
+            }
         }
     ) { innerPadding ->
         BodyContent(Modifier.padding(innerPadding))
     }
 }
+
 @Composable
-fun BodyContent(modifier: Modifier = Modifier){
-    Column(modifier = modifier){
-        Text(text = "Hi, there!")
-        Text(text = "Thanks for going through the layouts codelab")
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        ImageList()
     }
 }
 
 @Composable
-fun PhotographerCard(modifier: Modifier = Modifier){
+fun PhotographerCard(modifier: Modifier = Modifier) {
     Row(modifier
         .padding(8.dp)
         .clip(RoundedCornerShape(4.dp))
@@ -83,7 +147,7 @@ fun PhotographerCard(modifier: Modifier = Modifier){
                 .padding(start = 8.dp)
                 .align(Alignment.CenterVertically)
         ) {
-            Text(text = "Alfred Sisley",fontWeight = FontWeight.Bold)
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text(text = "3 minutos ago", style = MaterialTheme.typography.body2)
             }
@@ -91,7 +155,73 @@ fun PhotographerCard(modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+    Column(
+        Modifier
+            .verticalScroll(state = scrollState)
+            .fillMaxWidth()
+    ) {
+        repeat(100) {
+            Text(text = "Item $it")
+        }
+    }
+}
 
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val scrollState = rememberLazyListState()
+    val listSize = 100
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        Row(Modifier.fillMaxWidth()) {
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                },
+                modifier = Modifier.weight(0.5f)
+            ) {
+                Text(text = "Scroll on the top")
+            }
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                },
+                modifier = Modifier.weight(0.5f)
+            ) {
+                Text(text = "Scroll on the end")
+            }
+        }
+        LazyColumn(
+            state = scrollState,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(100) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
